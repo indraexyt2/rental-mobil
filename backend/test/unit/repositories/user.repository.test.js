0dfb6443f18e3be39f4297b18de2098a0db6134f
indrawansyah <indraexyt2@gmail.com>
2025-02-20 10:36:20 +0700

    test(auth): add unit test register new user

---
+++ backend/test/unit/repositories/user.repository.test.js
@@ -0,0 +1,46 @@
+import UserRepository from "../../../src/repositories/user.repository.js";
+
+jest.mock('../../../src/repositories/user.repository.js');
+
+const repository = new UserRepository();
+
+describe('User Repository', () => {
+    it('should can add user and return correct data', async () => {
+        const mockUser = {
+            "full_name": "Indra",
+            "email": "indra@example.com",
+            "password": "123"
+        };
+
+        const expectedUser = {
+            id: '1',
+            full_name: "Indra",
+            email: "indra@example.com",
+            phone: null,
+            address: null,
+            avatar: null,
+            role: null
+        };
+
+        repository.addUser.mockResolvedValue(expectedUser);
+
+        const newUser = await repository.addUser(mockUser);
+
+        expect(repository.addUser).toHaveBeenCalledTimes(1);
+        expect(repository.addUser).toHaveBeenCalledWith(mockUser);
+        expect(newUser).toEqual(expectedUser);
+    });
+
+    it('should throw an error when database operation fails', async () => {
+        const mockUser = {
+            full_name: "Indra",
+            email: "indra@example.com",
+            password: "123"
+        };
+
+        const errorMessage = "Database error";
+        repository.addUser.mockRejectedValue(new Error(errorMessage));
+
+        await expect(repository.addUser(mockUser)).rejects.toThrow(errorMessage);
+    });
+});
